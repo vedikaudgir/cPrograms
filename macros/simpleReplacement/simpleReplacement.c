3d06ce62ff48db7a34b8116cd06b1002d87f1a69
Vedika Udgir <vedikaudgir@gmail.com>
2025-04-19 19:01:56 +0530

    Add files via upload

---
+++ macros/simpleReplacement/simpleReplacement.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define PI 3.14
+#define EXPONENT 2.71
+#define BEGIN {
+#define END }
+#define print printf
+#define myFunction main
+#define TERMINATE ;
+
+void myFunction()
+    BEGIN
+        print("\n%.2f", PI) TERMINATE
+        print("\n%.2f", EXPONENT) TERMINATE
+    END
