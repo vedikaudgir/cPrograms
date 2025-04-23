112f2ad14053b7b014fa8ef54c5da38b42ce969c
Vedika Udgir <vedikaudgir@gmail.com>
2025-04-23 18:53:12 +0530

    Add files via upload

---
+++ CLIarguments/argcargv/calci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc != 4) {
+        printf("Usage: %s <num1> <operator> <num2>\n", argv[0]);
+        return 1;
+    }
+
+    int a = atoi(argv[1]);
+    int b = atoi(argv[3]);
+    char op = argv[2][0];
+
+    switch (op) {
+        case '+': printf("%d\n", a + b); break;
+        case '-': printf("%d\n", a - b); break;
+        case '*': printf("%d\n", a * b); break;
+        case '/': printf("%d\n", b != 0 ? a / b : 0); break;
+        default: printf("Unknown operator '%c'\n", op);
+    }
+    
+    return 0;
+}
